f658c54b74045359b664c50f3cc6720b40ca8720
Sercrecy <caetanotiaguinho@gmail.com>
2026-05-04 07:51:28 -0300

    Update seletor.py

--- seletor.py
+++ seletor.py
@@ -1,1 +1,8 @@
-
+#Seletor de Caracteres --
+letra = input("Digite uma Letra: ").upper()
+if letra == 'A':
+  print("Voce Selecionoua Opcão A")
+elif letra == 'B':
+  print("Voce Selecionou a Opcao B")
+else:
+ print("Essa Opcao Não está Disponivel")
